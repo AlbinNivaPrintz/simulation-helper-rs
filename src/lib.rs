62dc38396a563c231230efd9e2ec8cd60c62aa47
Albin Niva Printz <printz@abios.se>
2022-05-07 10:14:02 +0200

    Add progress bars!

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,111 @@
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressIterator};
+use rayon::prelude::*;
+
 pub trait SimulationEngine<T> {
-    fn run(&self) -> Vec<T>;
+    fn run(&mut self) -> Vec<T>;
+
+    fn with_n_simulations(&mut self, n: usize);
+
+    fn n_simulations(&self) -> usize;
+
+    fn with_progress_bar(&mut self, pb: ProgressBar);
+
+    fn with_default_progress_bar(&mut self) {
+        let pb = ProgressBar::new(self.n_simulations() as u64);
+        self.with_progress_bar(pb);
+    }
+
+    fn without_progress_bar(&mut self);
 }
 
 pub struct SerialSimulationEngine<T, F: Fn() -> T> {
-    n: u64,
+    n: usize,
     simulation: F,
+    pb: Option<ProgressBar>,
 }
 
 impl<T, F: Fn() -> T> SerialSimulationEngine<T, F> {
-    pub fn new(n: u64, simulation: F) -> SerialSimulationEngine<T, F> {
-        SerialSimulationEngine { n, simulation }
+    pub fn new(n: usize, simulation: F) -> SerialSimulationEngine<T, F> {
+        SerialSimulationEngine {
+            n,
+            simulation,
+            pb: None,
+        }
     }
 }
 
 impl<T, F: Fn() -> T> SimulationEngine<T> for SerialSimulationEngine<T, F> {
-    fn run(&self) -> Vec<T> {
-        (0..self.n).map(|_| (self.simulation)()).collect()
+    fn run(&mut self) -> Vec<T> {
+        let iter = 0..self.n;
+        if let Some(pb) = self.pb.take() {
+            iter.progress_with(pb)
+                .map(|_| (self.simulation)())
+                .collect()
+        } else {
+            iter.map(|_| (self.simulation)()).collect()
+        }
+    }
+
+    fn with_n_simulations(&mut self, n: usize) {
+        self.n = n
+    }
+
+    fn n_simulations(&self) -> usize {
+        self.n
+    }
+
+    fn with_progress_bar(&mut self, pb: ProgressBar) {
+        self.pb = Some(pb)
+    }
+
+    fn without_progress_bar(&mut self) {
+        self.pb = None
+    }
+}
+
+pub struct ParSimulationEngine<T: std::marker::Send, F: Fn() -> T + std::marker::Sync> {
+    n: usize,
+    simulation: F,
+    pb: Option<ProgressBar>,
+}
+
+impl<T: std::marker::Send, F: Fn() -> T + std::marker::Sync> ParSimulationEngine<T, F> {
+    pub fn new(n: usize, simulation: F) -> ParSimulationEngine<T, F> {
+        ParSimulationEngine {
+            n,
+            simulation,
+            pb: None,
+        }
+    }
+}
+
+impl<T: std::marker::Send, F: Fn() -> T + std::marker::Sync> SimulationEngine<T>
+    for ParSimulationEngine<T, F>
+{
+    fn run(&mut self) -> Vec<T> {
+        let iter = (0..self.n).into_par_iter();
+        if let Some(pb) = self.pb.take() {
+            iter.progress_with(pb)
+                .map(|_| (self.simulation)())
+                .collect()
+        } else {
+            iter.map(|_| (self.simulation)()).collect()
+        }
+    }
+
+    fn with_n_simulations(&mut self, n: usize) {
+        self.n = n
+    }
+
+    fn n_simulations(&self) -> usize {
+        self.n
+    }
+
+    fn with_progress_bar(&mut self, pb: ProgressBar) {
+        self.pb = Some(pb)
+    }
+
+    fn without_progress_bar(&mut self) {
+        self.pb = None
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 use rand::Rng;
-use simulator::{SerialSimulationEngine, SimulationEngine};
+use simulator::{ParSimulationEngine, SerialSimulationEngine, SimulationEngine};
+use std::time::Instant;
 
 fn main() {
-    let eng = SerialSimulationEngine::new(10, || {
-        let mut rng = rand::thread_rng();
-        if rng.gen::<f64>() > 0.5 {
-            1
-        } else {
-            0
-        }
-    });
-    let res = eng.run();
-    println!("{:?}", res);
+    // Demo usage
+    {
+        // SerialSimulationEngine runs simulations serially
+        let start = Instant::now();
+        let mut eng = SerialSimulationEngine::new(1000, || {
+            let mut rng = rand::thread_rng();
+            if rng.gen::<f64>() > 0.5 {
+                1
+            } else {
+                0
+            }
+        });
+        eng.with_default_progress_bar();
+        let res = eng.run();
+        println!("{:?} {:?}", start.elapsed(), res);
+    }
+    {
+        // ParSimulationEngine runs simulations in parallel using rayon
+        let start = Instant::now();
+        let mut eng = ParSimulationEngine::new(1000, || {
+            let mut rng = rand::thread_rng();
+            if rng.gen::<f64>() > 0.5 {
+                1
+            } else {
+                0
+            }
+        });
+        eng.with_default_progress_bar();
+        let res = eng.run();
+        println!("{:?} {:?}", start.elapsed(), res);
+    }
 }
